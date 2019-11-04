305e7292cc3976014b58bb7ede13177d1ef5865e
lclc98 <lclc98@users.noreply.github.com>
2019-11-04 16:57:39 +1000

    Initial Commit

---
+++ src/main/java/com/diluv/api/DiluvAPI.java
@@ -0,0 +1,15 @@
+package com.diluv.api;
+
+import java.util.logging.Logger;
+
+public class DiluvAPI {
+    private static final Logger LOGGER = Logger.getLogger(DiluvAPI.class.getName());
+
+    public static void main(String[] args) {
+        new DiluvAPI().start();
+    }
+
+    private void start() {
+    }
+
+}
