c929461758fdf476267ac0ec426193696020ce03
Tyler Hancock <darklime@live.ca>
2020-02-22 16:57:53 -0700

    Added documentation to authorized user data.

--- src/main/java/com/diluv/api/endpoints/v1/user/DataAuthorizedUser.java
+++ src/main/java/com/diluv/api/endpoints/v1/user/DataAuthorizedUser.java
@@ -3,11 +3,20 @@ package com.diluv.api.endpoints.v1.user;
 import com.diluv.confluencia.database.record.UserRecord;
 import com.google.gson.annotations.Expose;
 
+/**
+ * Represents data on a user from an authorized perspective.
+ */
 public class DataAuthorizedUser extends DataUser {
     
+    /**
+     * The email address of the user.
+     */
     @Expose
     private final String email;
     
+    /**
+     * Whether or not the account has MFA enabled.
+     */
     @Expose
     private final boolean mfa;
     
