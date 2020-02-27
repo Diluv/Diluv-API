6e041c4f0c8e2d80f748ab4e5dff5f13dc09e18a
Tyler Hancock <darklime@live.ca>
2020-02-26 19:27:46 -0700

    Cleaned up endpoint method names.

--- src/main/java/com/diluv/api/v1/games/GamesAPI.java
+++ src/main/java/com/diluv/api/v1/games/GamesAPI.java
@@ -1,5 +1,7 @@
 package com.diluv.api.v1.games;
 
+import static com.diluv.api.Main.DATABASE;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
@@ -28,9 +30,9 @@ import com.diluv.api.data.DataProjectFile;
 import com.diluv.api.data.DataProjectFileAvailable;
 import com.diluv.api.data.DataProjectFileInQueue;
 import com.diluv.api.data.DataProjectType;
-import com.diluv.api.utils.ResponseUtil;
 import com.diluv.api.utils.Constants;
 import com.diluv.api.utils.ImageUtil;
+import com.diluv.api.utils.ResponseUtil;
 import com.diluv.api.utils.auth.AccessToken;
 import com.diluv.api.utils.auth.Validator;
 import com.diluv.api.utils.error.ErrorMessage;
@@ -41,168 +43,166 @@ import com.diluv.confluencia.database.record.ProjectRecord;
 import com.diluv.confluencia.database.record.ProjectTypeRecord;
 import com.github.slugify.Slugify;
 
-import static com.diluv.api.Main.DATABASE;
-
 @Path("/games")
 public class GamesAPI {
-
+    
     private final Slugify slugify = new Slugify();
-
+    
     @GET
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getGames () {
-
+        
         final List<GameRecord> gameRecords = DATABASE.gameDAO.findAll();
         final List<DataGame> games = gameRecords.stream().map(DataGame::new).collect(Collectors.toList());
         return ResponseUtil.successResponse(games);
     }
-
+    
     @GET
     @Path("/{gameSlug}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getGameBySlug (@PathParam("gameSlug") String gameSlug) {
-
+    public Response getGame (@PathParam("gameSlug") String gameSlug) {
+        
         final GameRecord gameRecord = DATABASE.gameDAO.findOneBySlug(gameSlug);
         if (gameRecord == null) {
-
+            
             return ErrorMessage.NOT_FOUND_GAME.respond();
         }
         return ResponseUtil.successResponse(new DataGame(gameRecord));
     }
-
+    
     @GET
     @Path("/{gameSlug}/types")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getProjectTypesByGameSlug (@PathParam("gameSlug") String gameSlug) {
-
+    public Response getProjectTypes (@PathParam("gameSlug") String gameSlug) {
+        
         if (DATABASE.gameDAO.findOneBySlug(gameSlug) == null) {
-
+            
             return ErrorMessage.NOT_FOUND_GAME.respond();
         }
-
+        
         final List<ProjectTypeRecord> projectTypesRecords = DATABASE.projectDAO.findAllProjectTypesByGameSlug(gameSlug);
         final List<DataProjectType> projectTypes = projectTypesRecords.stream().map(DataProjectType::new).collect(Collectors.toList());
-
+        
         return ResponseUtil.successResponse(projectTypes);
     }
-
+    
     @GET
     @Path("/{gameSlug}/{projectTypeSlug}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getProjectTypesByGameSlugAndProjectType (@PathParam("gameSlug") String gameSlug, @PathParam("projectTypeSlug") String projectTypeSlug) {
-
+    public Response getProjectType (@PathParam("gameSlug") String gameSlug, @PathParam("projectTypeSlug") String projectTypeSlug) {
+        
         final ProjectTypeRecord projectTypesRecords = DATABASE.projectDAO.findOneProjectTypeByGameSlugAndProjectTypeSlug(gameSlug, projectTypeSlug);
-
+        
         if (projectTypesRecords == null) {
-
+            
             if (DATABASE.gameDAO.findOneBySlug(gameSlug) == null) {
-
+                
                 return ErrorMessage.NOT_FOUND_GAME.respond();
             }
-
+            
             return ErrorMessage.NOT_FOUND_PROJECT_TYPE.respond();
         }
-
+        
         return ResponseUtil.successResponse(new DataProjectType(projectTypesRecords));
     }
-
+    
     @GET
     @Path("/{gameSlug}/{projectTypeSlug}/projects")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getProjectsByGameSlugAndProjectType (@PathParam("gameSlug") String gameSlug, @PathParam("projectTypeSlug") String projectTypeSlug) {
-
+    public Response getProjects (@PathParam("gameSlug") String gameSlug, @PathParam("projectTypeSlug") String projectTypeSlug) {
+        
         final List<ProjectRecord> projectRecords = DATABASE.projectDAO.findAllProjectsByGameSlugAndProjectType(gameSlug, projectTypeSlug);
-
+        
         if (projectRecords.isEmpty()) {
-
+            
             if (DATABASE.gameDAO.findOneBySlug(gameSlug) == null) {
-
+                
                 return ErrorMessage.NOT_FOUND_GAME.respond();
             }
-
+            
             if (DATABASE.projectDAO.findOneProjectTypeByGameSlugAndProjectTypeSlug(gameSlug, projectTypeSlug) == null) {
-
+                
                 return ErrorMessage.NOT_FOUND_PROJECT_TYPE.respond();
             }
         }
-
+        
         final List<DataProject> projects = projectRecords.stream().map(DataProject::new).collect(Collectors.toList());
         return ResponseUtil.successResponse(projects);
     }
-
+    
     @GET
     @Path("/{gameSlug}/{projectTypeSlug}/{projectSlug}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getProjectByGameSlugAndProjectTypeAndProjectSlug (@HeaderParam("Authorization") String auth, @PathParam("gameSlug") String gameSlug, @PathParam("projectTypeSlug") String projectTypeSlug, @PathParam("projectSlug") String projectSlug) {
-
+    public Response getProject (@HeaderParam("Authorization") String auth, @PathParam("gameSlug") String gameSlug, @PathParam("projectTypeSlug") String projectTypeSlug, @PathParam("projectSlug") String projectSlug) {
+        
         final AccessToken token = AccessToken.getToken(auth);
-
+        
         final ProjectRecord projectRecord = DATABASE.projectDAO.findOneProjectByGameSlugAndProjectTypeSlugAndProjectSlug(gameSlug, projectTypeSlug, projectSlug);
         if (projectRecord == null || !projectRecord.isReleased() && token == null) {
             if (DATABASE.gameDAO.findOneBySlug(gameSlug) == null) {
-
+                
                 return ErrorMessage.NOT_FOUND_GAME.respond();
             }
-
+            
             if (DATABASE.projectDAO.findOneProjectTypeByGameSlugAndProjectTypeSlug(gameSlug, projectTypeSlug) == null) {
-
+                
                 return ErrorMessage.NOT_FOUND_PROJECT_TYPE.respond();
             }
-
+            
             return ErrorMessage.NOT_FOUND_PROJECT.respond();
         }
-
+        
         final List<ProjectAuthorRecord> records = DATABASE.projectDAO.findAllProjectAuthorsByProjectId(projectRecord.getId());
-
+        
         if (token != null) {
-
+            
             List<String> permissions = null;
             if (token.getUserId() == projectRecord.getUserId()) {
-                //TODO All permissions
+                // TODO All permissions
                 permissions = new ArrayList<>();
             }
             else {
                 final Optional<ProjectAuthorRecord> record = records.stream().filter(par -> par.getUserId() == token.getUserId()).findFirst();
-
+                
                 if (record.isPresent()) {
                     permissions = record.get().getPermissions();
                 }
             }
-
+            
             if (permissions != null) {
                 final List<DataProjectContributor> projectAuthors = records.stream().map(DataProjectAuthorAuthorized::new).collect(Collectors.toList());
                 return ResponseUtil.successResponse(new DataProjectAuthorized(projectRecord, projectAuthors, permissions));
             }
         }
-
+        
         final List<DataProjectContributor> projectAuthors = records.stream().map(DataProjectContributor::new).collect(Collectors.toList());
         return ResponseUtil.successResponse(new DataProject(projectRecord, projectAuthors));
     }
-
+    
     @GET
     @Path("/{gameSlug}/{projectTypeSlug}/{projectSlug}/files")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getProjectFilesByGameSlugAndProjectTypeAndProjectSlug (@HeaderParam("Authorization") String auth, @PathParam("gameSlug") String gameSlug, @PathParam("projectTypeSlug") String projectTypeSlug, @PathParam("projectSlug") String projectSlug) {
-
+    public Response getProjectFiles (@HeaderParam("Authorization") String auth, @PathParam("gameSlug") String gameSlug, @PathParam("projectTypeSlug") String projectTypeSlug, @PathParam("projectSlug") String projectSlug) {
+        
         final AccessToken token = AccessToken.getToken(auth);
-
+        
         final ProjectRecord projectRecord = DATABASE.projectDAO.findOneProjectByGameSlugAndProjectTypeSlugAndProjectSlug(gameSlug, projectTypeSlug, projectSlug);
         if (projectRecord == null) {
-
+            
             if (DATABASE.gameDAO.findOneBySlug(gameSlug) == null) {
                 return ErrorMessage.NOT_FOUND_GAME.respond();
             }
-
+            
             if (DATABASE.projectDAO.findOneProjectTypeByGameSlugAndProjectTypeSlug(gameSlug, projectTypeSlug) == null) {
                 return ErrorMessage.NOT_FOUND_PROJECT_TYPE.respond();
             }
-
+            
             return ErrorMessage.NOT_FOUND_PROJECT.respond();
         }
-
+        
         List<ProjectFileRecord> projectRecords;
-
+        
         // TODO Check permissions
         if (token != null && projectRecord.getUserId() == token.getUserId()) {
             projectRecords = DATABASE.fileDAO.findAllByGameSlugAndProjectTypeAndProjectSlugAuthorized(gameSlug, projectTypeSlug, projectSlug);
@@ -221,151 +221,151 @@ public class GamesAPI {
         }
         return ResponseUtil.successResponse(projects);
     }
-
+    
     @POST
     @Path("/{gameSlug}/{projectTypeSlug}")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response postProjectByGameSlugAndProjectType (@HeaderParam("Authorization") String auth,
-                                                         @PathParam("gameSlug") String gameSlug,
-                                                         @PathParam("projectTypeSlug") String projectTypeSlug,
-                                                         @MultipartForm ProjectCreateForm form) {
-//
+    public Response postProject (@HeaderParam("Authorization") String auth, @PathParam("gameSlug") String gameSlug, @PathParam("projectTypeSlug") String projectTypeSlug, @MultipartForm ProjectCreateForm form) {
+        
+        //
         final AccessToken token = AccessToken.getToken(auth);
         if (token == null) {
             return ErrorMessage.USER_REQUIRED_TOKEN.respond();
         }
-
+        
         if (DATABASE.gameDAO.findOneBySlug(gameSlug) == null) {
             return ErrorMessage.NOT_FOUND_GAME.respond();
         }
-
+        
         if (DATABASE.projectDAO.findOneProjectTypeByGameSlugAndProjectTypeSlug(gameSlug, projectTypeSlug) == null) {
             return ErrorMessage.NOT_FOUND_PROJECT_TYPE.respond();
         }
-//1000000L
+        // 1000000L
         // Defaults to 1MB should be database stored. TODO
-//        final FormData.FileItem formLogo = RequestUtil.getFormFile(data, "logo");
-
+        // final FormData.FileItem formLogo = RequestUtil.getFormFile(data, "logo");
+        
         if (!Validator.validateProjectName(form.name)) {
             return ErrorMessage.PROJECT_INVALID_NAME.respond();
         }
-
+        
         if (!Validator.validateProjectSummary(form.summary)) {
             return ErrorMessage.PROJECT_INVALID_SUMMARY.respond();
         }
-
+        
         if (!Validator.validateProjectDescription(form.description)) {
             return ErrorMessage.PROJECT_INVALID_DESCRIPTION.respond();
         }
-
+        
         if (form.logo == null) {
             return ErrorMessage.PROJECT_INVALID_LOGO.respond();
         }
-
+        
         final BufferedImage image = ImageUtil.isValidImage(form.logo);
-
+        
         if (image == null) {
             return ErrorMessage.PROJECT_INVALID_LOGO.respond();
         }
         final String projectSlug = this.slugify.slugify(form.name);
-
+        
         if (DATABASE.projectDAO.findOneProjectByGameSlugAndProjectTypeSlugAndProjectSlug(gameSlug, projectTypeSlug, projectSlug) != null) {
             return ErrorMessage.PROJECT_TAKEN_SLUG.respond();
         }
         if (!DATABASE.projectDAO.insertProject(projectSlug, form.name, form.summary, form.summary, token.getUserId(), gameSlug, projectTypeSlug)) {
             return ErrorMessage.FAILED_CREATE_PROJECT.respond();
         }
-
+        
         final ProjectRecord projectRecord = DATABASE.projectDAO.findOneProjectByGameSlugAndProjectTypeSlugAndProjectSlug(gameSlug, projectTypeSlug, projectSlug);
-
+        
         if (projectRecord == null) {
             return ErrorMessage.NOT_FOUND_PROJECT.respond();
         }
-
+        
         final File file = new File(Constants.CDN_FOLDER, String.format("games/%s/%s/%s/logo.png", gameSlug, projectTypeSlug, projectSlug));
         if (!ImageUtil.saveImage(image, file)) {
             return ErrorMessage.ERROR_SAVING_IMAGE.respond();
         }
         return ResponseUtil.successResponse(new DataProject(projectRecord));
     }
-
+    
     @POST
     @Path("/{gameSlug}/{projectTypeSlug}/{projectSlug}/files")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response postProjectFilesByGameSlugAndProjectTypeAndProjectSlug (@HeaderParam("Authorization") String auth,
-                                                                            @PathParam("gameSlug") String gameSlug,
-                                                                            @PathParam("projectTypeSlug") String projectTypeSlug,
-                                                                            @PathParam("projectSlug") String projectSlug,
-                                                                            @MultipartForm ProjectFileUploadForm form) {
-
+    public Response postProjectFile (@HeaderParam("Authorization") String auth, @PathParam("gameSlug") String gameSlug, @PathParam("projectTypeSlug") String projectTypeSlug, @PathParam("projectSlug") String projectSlug, @MultipartForm ProjectFileUploadForm form) {
+        
         final AccessToken token = AccessToken.getToken(auth);
-
+        
         if (token == null) {
             return ErrorMessage.USER_REQUIRED_TOKEN.respond();
         }
-
+        
         if (DATABASE.gameDAO.findOneBySlug(gameSlug) == null) {
             return ErrorMessage.NOT_FOUND_GAME.respond();
         }
-
+        
         final ProjectTypeRecord projectTypeRecord = DATABASE.projectDAO.findOneProjectTypeByGameSlugAndProjectTypeSlug(gameSlug, projectTypeSlug);
         if (projectTypeRecord == null) {
             return ErrorMessage.NOT_FOUND_PROJECT_TYPE.respond();
         }
-
+        
         final ProjectRecord projectRecord = DATABASE.projectDAO.findOneProjectByGameSlugAndProjectTypeSlugAndProjectSlug(gameSlug, projectTypeSlug, projectSlug);
         if (projectRecord == null) {
             return ErrorMessage.NOT_FOUND_PROJECT_TYPE.respond();
         }
-
+        
         // TODO Needs to be moved to check the user permissions in the future
         if (projectRecord.getUserId() != token.getUserId()) {
             return ErrorMessage.USER_NOT_AUTHORIZED.respond();
         }
-//        final FormData data = FormUtil.getMultiPartForm(exchange, projectTypeRecord.getMaxSize());
-
-//        if (data == null) {
-//            return ErrorMessage.FORM_INVALID.respond();
-//        }
-
-//        final FormData.FileItem formFile = RequestUtil.getFormFile(data, "file");
-
+        // final FormData data = FormUtil.getMultiPartForm(exchange,
+        // projectTypeRecord.getMaxSize());
+        
+        // if (data == null) {
+        // return ErrorMessage.FORM_INVALID.respond();
+        // }
+        
+        // final FormData.FileItem formFile = RequestUtil.getFormFile(data, "file");
+        
         if (!Validator.validateProjectFileChangelog(form.changelog)) {
             return ErrorMessage.PROJECT_FILE_INVALID_CHANGELOG.respond();
         }
-
-//        if (formFile == null) {
-//            return ErrorMessage.PROJECT_FILE_INVALID_FILE.respond();
-//        }
-//
-//        final Long fileSize = FileUtil.getSize(formFile);
-//
-//        final String sha512 = FileUtil.getSHA512(formFile);
-//        final String fileName = formFile.getFile().getFileName().toString();
-
-//        if (sha512 == null) {
-//            return ErrorMessage.FAILED_SHA512.respond();
-//        }
-
-//        final Long id = DATABASE.fileDAO.insertProjectFile(fileName, fileSize, form.changelog, sha512, projectRecord.getId(), token.getUserId());
-//        if (id == null) {
-//            return ErrorMessage.FAILED_CREATE_PROJECT_FILE.respond();
-//        }
-//
-//        final File file = new File(Constants.PROCESSING_FOLDER, String.format("%s/%s/%s/%s/%s", gameSlug, projectTypeSlug, projectSlug, id, fileName));
-//        try {
-//
-//            FileUtils.copyInputStreamToFile(form.file, file);
-//        }
-//        catch (final IOException e) {
-//            DiluvAPIServer.LOGGER.error("Failed to postProjectFilesByGameSlugAndProjectTypeAndProjectSlug.", e);
-//            return ErrorMessage.ERROR_WRITING.respond();
-//        }
-
-        //TODO
-//        final ProjectFileRecord record = DATABASE.fileDAO.findOneProjectFileQueueByFileId(id);
-        return ResponseUtil.successResponse(new DataProjectFileInQueue(/*record*/null, gameSlug, projectTypeSlug, projectSlug));
+        
+        // if (formFile == null) {
+        // return ErrorMessage.PROJECT_FILE_INVALID_FILE.respond();
+        // }
+        //
+        // final Long fileSize = FileUtil.getSize(formFile);
+        //
+        // final String sha512 = FileUtil.getSHA512(formFile);
+        // final String fileName = formFile.getFile().getFileName().toString();
+        
+        // if (sha512 == null) {
+        // return ErrorMessage.FAILED_SHA512.respond();
+        // }
+        
+        // final Long id = DATABASE.fileDAO.insertProjectFile(fileName, fileSize,
+        // form.changelog, sha512, projectRecord.getId(), token.getUserId());
+        // if (id == null) {
+        // return ErrorMessage.FAILED_CREATE_PROJECT_FILE.respond();
+        // }
+        //
+        // final File file = new File(Constants.PROCESSING_FOLDER,
+        // String.format("%s/%s/%s/%s/%s", gameSlug, projectTypeSlug, projectSlug, id,
+        // fileName));
+        // try {
+        //
+        // FileUtils.copyInputStreamToFile(form.file, file);
+        // }
+        // catch (final IOException e) {
+        // DiluvAPIServer.LOGGER.error("Failed to
+        // postProjectFilesByGameSlugAndProjectTypeAndProjectSlug.", e);
+        // return ErrorMessage.ERROR_WRITING.respond();
+        // }
+        
+        // TODO
+        // final ProjectFileRecord record =
+        // DATABASE.fileDAO.findOneProjectFileQueueByFileId(id);
+        return ResponseUtil.successResponse(new DataProjectFileInQueue(/* record */null, gameSlug, projectTypeSlug, projectSlug));
     }
 }
