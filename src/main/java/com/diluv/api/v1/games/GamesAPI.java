da0207b1438f3aae682b5c42c05d3be0cad16582
lclc98 <lclc98@lclc98.com>
2020-05-06 16:04:45 +1000

    Changed data shown in feed

--- src/main/java/com/diluv/api/v1/games/GamesAPI.java
+++ src/main/java/com/diluv/api/v1/games/GamesAPI.java
@@ -1,28 +1,5 @@
 package com.diluv.api.v1.games;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.jboss.resteasy.annotations.GZIP;
-import org.jboss.resteasy.annotations.cache.Cache;
-import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
-import org.jboss.resteasy.plugins.providers.atom.Entry;
-import org.jboss.resteasy.plugins.providers.atom.Feed;
-
 import com.diluv.api.data.*;
 import com.diluv.api.utils.Constants;
 import com.diluv.api.utils.ImageUtil;
@@ -33,19 +10,34 @@ import com.diluv.api.utils.error.ErrorMessage;
 import com.diluv.api.utils.permissions.ProjectPermissions;
 import com.diluv.api.utils.response.ResponseUtil;
 import com.diluv.api.v1.games.feed.FeedProject;
-import com.diluv.confluencia.database.record.CategoryRecord;
-import com.diluv.confluencia.database.record.GameRecord;
-import com.diluv.confluencia.database.record.GameVersionRecord;
-import com.diluv.confluencia.database.record.ProjectAuthorRecord;
-import com.diluv.confluencia.database.record.ProjectFileRecord;
-import com.diluv.confluencia.database.record.ProjectLinkRecord;
-import com.diluv.confluencia.database.record.ProjectRecord;
-import com.diluv.confluencia.database.record.ProjectTypeRecord;
+import com.diluv.confluencia.database.record.*;
 import com.diluv.confluencia.database.sort.GameSort;
 import com.diluv.confluencia.database.sort.NewsSort;
 import com.diluv.confluencia.database.sort.ProjectFileSort;
 import com.diluv.confluencia.database.sort.ProjectSort;
 import com.github.slugify.Slugify;
+
+import org.jboss.resteasy.annotations.GZIP;
+import org.jboss.resteasy.annotations.cache.Cache;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+import org.jboss.resteasy.plugins.providers.atom.Content;
+import org.jboss.resteasy.plugins.providers.atom.Entry;
+import org.jboss.resteasy.plugins.providers.atom.Feed;
+import org.jboss.resteasy.plugins.providers.atom.Link;
+import org.jboss.resteasy.plugins.providers.atom.Person;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.diluv.api.Main.DATABASE;
 
@@ -177,7 +169,7 @@ public class GamesAPI {
 
     @Cache(maxAge = 30, mustRevalidate = true)
     @GET
-    @Path("/{gameSlug}/{projectTypeSlug}/feed.xml")
+    @Path("/{gameSlug}/{projectTypeSlug}/feed.atom")
     @Produces(MediaType.APPLICATION_ATOM_XML)
     public Feed getProjectFeed (@HeaderParam("Authorization") Token token, @PathParam("gameSlug") String gameSlug, @PathParam("projectTypeSlug") String projectTypeSlug) {
 
@@ -196,14 +188,22 @@ public class GamesAPI {
 ////            }
 //        }
 
+        final String baseUrl = String.format("%s/games/%s/%s", Constants.WEBSITE_URL, gameSlug, projectTypeSlug);
         Feed feed = new Feed();
+        feed.setId(URI.create(baseUrl + "/feed.atom"));
+        feed.getLinks().add(new Link("self", baseUrl));
+        feed.setUpdated(new Date());
+        feed.setTitle("Project Feed");
         for (ProjectRecord record : projectRecords) {
-            Entry entity = new Entry();
-
-            final List<CategoryRecord> categoryRecords = DATABASE.projectDAO.findAllCategoriesByProjectId(record.getId());
-            List<DataCategory> categories = categoryRecords.stream().map(DataCategory::new).collect(Collectors.toList());
-            entity.setAnyOtherJAXBObject(new FeedProject(record, categories));
-            feed.getEntries().add(entity);
+            Content content = new Content();
+            content.setText(record.getSummary());
+            Entry entry = new Entry();
+            entry.setId(URI.create(baseUrl + "/" + record.getSlug()));
+            entry.setTitle(record.getName());
+            entry.setUpdated(new Date(record.getUpdatedAt()));
+            entry.setContent(content);
+            entry.getAuthors().add(new Person(record.getUserDisplayName()));
+            feed.getEntries().add(entry);
         }
 
 
